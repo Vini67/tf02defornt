6e99f8cc65f169a77058dd1a0fdb06fc613d4570
Vini <Vinibernardes@outlook.com>
2025-09-01 20:01:24 -0300

    Projeto Aula 02

--- public/js/app.js
+++ public/js/app.js
@@ -7,5 +7,29 @@ liElementCollection.forEach((liElement) => {
 
 
         /** CODAR AQUI */
+    document.addEventListener("DOMContentLoaded", () => {
+  const listItems = document.querySelectorAll(".content ul.demo>li");
+
+  listItems.forEach(item => {
+    item.addEventListener("click", (event) => {
+      const li = event.target.closest("li"); // garante pegar o li correto
+      if (!li) return;
+
+      // Remove .selected de todos
+      listItems.forEach(el => el.classList.remove("selected"));
+
+      // Adiciona .selected no li clicado
+      li.classList.add("selected");
+    });
+  });
+});
+
+
+
+
+
+
+
+        
     });
 });
